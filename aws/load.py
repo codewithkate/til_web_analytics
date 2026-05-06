465350cff89b78f534f7fe6865bccd57b91ff74a
Kate Crawford <kaitlencrawford@gmail.com>
2026-05-06 10:15:00 +0100

    update gitignore. refactor load.py to use functions. test upload. run upload.

--- aws/load.py
+++ aws/load.py
@@ -2,7 +2,7 @@
 Data Loading into a S3 bucket using boto3
 
 Requirements:
-    - personlized aws templates
+    - personlize aws templates
     - run commands and record outputs in the .aws_env file
         - create bucket
         - create policy
@@ -13,22 +13,54 @@ Requirements:
 import os
 import boto3
 import logging
+from dotenv import load_dotenv
 from datetime import datetime
 from pathlib import Path
 
+def configure_logs(format='%(asctime)s - %(levelname)s - %(message)s'):
+    log_dir = 'logs'
+    os.makedirs(log_dir, exist_ok=True)
+    timestamp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+    log_filename = f'{log_dir}/load_{timestamp}.log'
+
+    logging.basicConfig(
+        filename=log_filename,
+        format=format,    # format as time, level, and message
+        level=logging.INFO                                      # lowest level you want to capture with logs
+    )
+    return True
+
+
+def upload_to_s3(access_key, secret_key, region, bucket, local_file_location="test_upload.json", aws_object_destination="python-import/test_upload.json"):
+    """Upload a file to an S3 bucket
+
+    :param bucket: Bucket to upload to
+    :param local_file_location: File to upload
+    :param aws_object_destination: S3 object name. If not specified then file_name is used
+    :return: True if file was uploaded, else False
+    """
+
+    # If S3 object_name was not specified, use file_name
+    if aws_object_destination is None:
+        aws_object_destination = os.path.basename(local_file_location)
+
+    # Upload the file
+    s3_client = boto3.client(
+        service_name='s3',
+        region_name=region,
+        aws_access_key_id=access_key,
+        aws_secret_access_key=secret_key
+    )
+    try:
+        response = s3_client.upload_file(local_file_location, bucket, aws_object_destination)
+        logger.info(f'{aws_object_destination} uploaded to S3')
+    except Exception as e:
+        logging.error(e)
+        return False
+    return True
 
 # Configure logs
-log_dir = 'logs'
-os.makedirs(log_dir, exist_ok=True)
-timestamp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
-log_filename = f'{log_dir}/load_{timestamp}.log'
-
-logging.basicConfig(
-    filename=log_filename,
-    format='%(asctime)s - %(levelname)s - %(message)s',    # format as time, level, and message
-    level=logging.INFO                                      # lowest level you want to capture with logs
-)
-
+configure_logs()
 logger = logging.getLogger()
 logger.info('Logger initialized')
 
@@ -36,33 +68,33 @@ logger.info('Logger initialized')
 load_dotenv()
 
 # Store access credentials
-AWS_ACCESS_KEY = os.getenv('AWS_ACCESS_KEY_ID')
-AWS_SECRET_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+aws_access_key = os.getenv('AWS_ACCESS_KEY')
+aws_secret_key = os.getenv('AWS_SECRET_KEY')
+region = os.getenv('AWS_REGION')
 bucket = os.getenv('AWS_BUCKET_NAME')
 
-# Create a boto3 client/resource
-s3_client = boto3.client(
-    's3',
-    aws_access_key_id=AWS_ACCESS_KEY,
-    aws_secret_access_key=AWS_SECRET_KEY
-)
+# Get amplitude event files
+data_dir = Path('../amplitude/data')
+local_files = list(data_dir.glob('*.json'))
 
-# Upload amplitude events to the S3 bucket
-data_dir = Path('amplitude/data')
-files = list(data_dir.glob('*.json'))
-
+# Upload files to the S3 bucket
 processed = 0
 
-for file in files:
-    filename = os.path.basename(file)
+for file in local_files:
+    aws_object_destination = f'python-import/{os.path.basename(file)}'
     try:
-        s3_client.upload_file(file, bucket, filename)
-        logger.info(f'{file} uploaded to S3')
-        s3_client.head_object(Bucket=bucket, Key=filename) # only return metadata
-        os.remove(file)
-        logger.info(f'{file} deleted')
+        result = upload_to_s3(
+            access_key=aws_access_key, 
+            secret_key=aws_secret_key, 
+            region=region, 
+            bucket=bucket, 
+            local_file_location=file, 
+            aws_object_destination=aws_object_destination
+        )
+        # os.remove(file)
         processed+=1
     except Exception as e:
         logging.error(e)
 
-logging.info(f'{processed} files uploaded')
+logging.info(f'{processed} files uploaded to {bucket}')
+
